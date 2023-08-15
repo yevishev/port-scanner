d5029f3412f7e224d3fadf6c691d4d54e850ed83
yevishev <yevishev.m@gmail.com>
2023-08-15 17:31:45 +0300

    adding goroutines and channel

--- main.go
+++ main.go
@@ -12,16 +12,25 @@ func main() {
 	targetHost := "127.0.0.1"
 	startPort := 1
 	endPort := 65536
-	fmt.Println("start scanning")
+	ch := make(chan struct{}, endPort)
+	fmt.Println("start of scanning")
 
 	for port := startPort; port < endPort; port++ {
-		address := fmt.Sprintf("%s:%d", targetHost, port)
-		conn, err := net.DialTimeout("tcp", address, 1 * time.Second)
-		if err != nil {
-			continue
-		}
-		fmt.Printf("Port - %d - opened\n", port)
-		conn.Close()
+		go func (port int)  {
+			defer func ()  {
+				ch <- struct{}{}
+			}()
+			address := fmt.Sprintf("%s:%d", targetHost, port)
+			conn, err := net.DialTimeout("tcp", address, 1 * time.Second)
+			if err != nil {
+				return
+			}
+			fmt.Printf("Port - %d - opened\n", port)
+			conn.Close()
+		}(port)
+	}
+	for i := 1; i < endPort; i++ {
+		<- ch
 	}
 	fmt.Printf("end of scanning. time: %.2f\n", time.Since(t).Seconds())
 }
